69df5735a7318e516155aa2be55f4038448eb49d
asdf <asdf@gmail.com>
2023-07-10 21:20:05 +0900

    [김인규]  - mybatis insert 예제

--- src/main/java/com/marondal/spring/ex/mybatis/ReviewController.java
+++ src/main/java/com/marondal/spring/ex/mybatis/ReviewController.java
@@ -10,6 +10,7 @@ import com.marondal.spring.ex.mybatis.domain.Review;
 import com.marondal.spring.ex.mybatis.service.ReviewService;
 
 @Controller
+@RequestMapping("/mybatis")
 public class ReviewController {
 	
 	@Autowired
@@ -17,12 +18,35 @@ public class ReviewController {
 	
 	// id가 3인 review 데이터 조회 
 	
-	@RequestMapping("/mybatis/review")
+	@RequestMapping("/review")
 	@ResponseBody
 	public Review review(@RequestParam("id") int id) {
 		Review review = reviewService.getReview(id);
 		
 		return review;
 	}
+	
+	// 새로운 리뷰를 등록하는 기능
+	@RequestMapping("/ex02")
+	@ResponseBody
+	public String createReview() {
+		// 4, 콤비네이션 피자, 김인규, 4.5, 할인도 많이 받고잘 먹었어요. 
+//		int count = reviewService.addReview(4, "콤비네이션 피자", "김인규", 4.5, "할인도 많이 받고 잘 먹었어요");
+		
+		// 2, 뿌링클, 김인규, 4.0 역시 뿌링클은 진리 입니다. 
+		Review review = new Review();
+		review.setStoreId(2);
+		review.setMenu("뿌링클");
+		review.setUserName("김인규");
+		review.setPoint(4.0);
+		review.setReview("역시 뿌링클은 진리 입니다.");
+		
+		int count = reviewService.addReviewByObject(review);
+		
+	
+		return "삽입 개수 : " + count;
+		
+	}
+	
 
 }
