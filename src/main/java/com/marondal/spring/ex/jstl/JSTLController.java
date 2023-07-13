d3c568bea58868082bd7eaaaf18f9b78814f4421
asdf <asdf@gmail.com>
2023-07-13 20:31:18 +0900

    [김인규]  - jstl 코어라이브러리 변수, if

---
+++ src/main/java/com/marondal/spring/ex/jstl/JSTLController.java
@@ -0,0 +1,14 @@
+package com.marondal.spring.ex.jstl;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class JSTLController {
+	
+	@GetMapping("/jstl/ex01")
+	public String ex01() {
+		return "jstl/ex01";
+	}
+
+}
