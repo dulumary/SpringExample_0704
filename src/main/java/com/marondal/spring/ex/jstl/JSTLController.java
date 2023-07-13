1684caed1fc5325b2e2b5f72637c51c004ad3c7e
asdf <asdf@gmail.com>
2023-07-13 21:47:09 +0900

    [김인규]  - if else 반복문 예제 일부

--- src/main/java/com/marondal/spring/ex/jstl/JSTLController.java
+++ src/main/java/com/marondal/spring/ex/jstl/JSTLController.java
@@ -1,14 +1,33 @@
 package com.marondal.spring.ex.jstl;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/jstl")
 @Controller
 public class JSTLController {
 	
-	@GetMapping("/jstl/ex01")
+	@GetMapping("/ex01")
 	public String ex01() {
 		return "jstl/ex01";
+	}
+	
+	@GetMapping("/ex02")
+	public String ex02(Model model) {
+		
+		List<String> fruitList = new ArrayList<>();
+		fruitList.add("사과");
+		fruitList.add("딸기");
+		fruitList.add("바나나");
+		
+		model.addAttribute("fruitList", fruitList);
+		
+		return "jstl/ex02";
 	}
 
 }
