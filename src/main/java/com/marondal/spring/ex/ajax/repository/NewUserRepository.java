ddbcb1bd1dc97f7b29240efe36ebe494ab8258d2
asdf <asdf@gmail.com>
2023-07-19 21:49:11 +0900

    [김인규]  - ajax 예제 진행중

---
+++ src/main/java/com/marondal/spring/ex/ajax/repository/NewUserRepository.java
@@ -0,0 +1,21 @@
+package com.marondal.spring.ex.ajax.repository;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import com.marondal.spring.ex.jsp.domain.User;
+
+@Repository
+public interface NewUserRepository {
+	
+	public List<User> selectUserList();
+	
+	public int insertUser(
+			@Param("name") String name
+			, @Param("birthday") String birthday
+			, @Param("email") String email
+			, @Param("introduce") String introduce);
+
+}
